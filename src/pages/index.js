2ba3564ce3608ed84077a0fa57165c8edf0f1ed2
Aziz Ramos <sonicthehamilton.ar@gmail.com>
2020-11-05 23:17:26 -0500

    updating flyer width and favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ import SEO from '~/components/seo'
 const Wrapper = styled.div``
 const EventFlyer = styled.img`
   display: block;
+  max-width: 640px;
+  margin: auto;
   width: 100%;
 `
 
@@ -15,7 +17,15 @@ const IndexPage = () => (
   <>
     <SEO title="Home" keywords={[`Hanz`, `Gran Santo`, `Lake side vagabond`]} />
     <Wrapper>
-      <EventFlyer src={Flyer} />
+      <p style={{ textAlign: 'center', textTransform: 'uppercase' }}>
+        Click flyer below to get tickets!
+      </p>
+      <a
+        href="https://www.eventbrite.com/e/vagabond-musuem-the-score-art-installation-tickets-128073977715"
+        target="_blank"
+      >
+        <EventFlyer src={Flyer} />
+      </a>
     </Wrapper>
   </>
 )
